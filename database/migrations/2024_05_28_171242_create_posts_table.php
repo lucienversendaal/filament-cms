3dd79d6461b09889d3394ae431b9bc01475023c4
lucienversendaal <lucienversendaal07@gmail.com>
2024-05-28 19:33:52 +0200

    added default and nullable

--- database/migrations/2024_05_28_171242_create_posts_table.php
+++ database/migrations/2024_05_28_171242_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->default(null)->nullable();
             $table->timestamps();
         });
     }
